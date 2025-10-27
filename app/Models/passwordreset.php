8c80dda44bf3b85cd4b43cdd23579cbb917c799d
makindu <72305144+makindu@users.noreply.github.com>
2025-10-27 18:43:22 +0200

    model password resets updated and authController

--- app/Models/passwordreset.php
+++ app/Models/passwordreset.php
@@ -34,30 +34,29 @@ return $this->created_at < now()->subMinutes($minutes);
 */
 public static function generateOTP($email, $minutes = 15)
 {
-// Supprime les anciens OTP expirés pour cet email
-self::where('email', $email)
-->where('created_at', '<', Carbon::now()->subMinutes($minutes))
-->delete();
+    // Supprime les anciens OTP expirés pour cet email
+    self::where('email', $email)
+    ->where('created_at', '<', Carbon::now()->subMinutes($minutes))
+    ->delete();
 
-// Génération d'un code unique
-do {
-$code = rand(100000, 999999);
-$exists = self::where('code', $code)
-->where('created_at', '>=', Carbon::now()->subMinutes($minutes))
-->exists();
-} while ($exists);
+    // Génération d'un code unique
+    do {
+        $code = rand(100000, 999999);
+        $exists = self::where('code', $code)
+        ->where('created_at', '>=', Carbon::now()->subMinutes($minutes))
+        ->exists();
+    } while ($exists);
 
-// Génération du token sécurisé
-$token = bcrypt(Str::random(60));
+    // Génération du token sécurisé
+    $token = bcrypt(Str::random(60));
 
-// Stockage ou mise à jour
-return self::updateOrCreate(
-['email' => $email],
-[
-'code' => $code,
-'token' => $token,
-'created_at' => now(),
-]
-);
-}
+    // Stockage ou mise à jour
+    return self::updateOrCreate(
+        ['email' => $email],
+        [
+        'code' => $code,
+        'token' => $token,
+        'created_at' => now(),
+        ]);
+    }
 }
